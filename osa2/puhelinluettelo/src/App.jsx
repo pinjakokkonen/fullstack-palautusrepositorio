d4cf533d37752b37af3d8a7803e822232a6a8dd8
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-05-24 17:51:46 +0300

    2.6, 2.7: puhelinluettelo

---
+++ osa2/puhelinluettelo/src/App.jsx
@@ -0,0 +1,51 @@
+import { useState } from 'react'
+
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ]) 
+  const [newName, setNewName] = useState('')
+
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const addName = (event) => {
+    event.preventDefault()
+    let status=true
+    let i, len=''
+    for (i=0, len=persons.length; i<len; i++) {
+      if (persons[i].name == newName) {
+        status=false
+        alert(`${newName} is already added to phonebook`)
+      }
+    }
+    const nameObject = {
+      name: newName
+    }
+    if (status) {
+      setPersons(persons.concat(nameObject))
+    }
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={addName}>
+        <div>
+          name: <input value={newName} onChange={handleNameChange}/>
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map(person => <p key={person.name}>{person.name}</p>)}
+    </div>
+  )
+
+}
+
+export default App
