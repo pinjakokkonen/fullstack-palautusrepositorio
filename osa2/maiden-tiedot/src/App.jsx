d8e958c9b7f5be8727989e3b74a3935be21fedc3
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-08-07 14:04:12 +0300

    2.20: maiden tiedot

--- osa2/maiden-tiedot/src/App.jsx
+++ osa2/maiden-tiedot/src/App.jsx
@@ -1,5 +1,6 @@
 import { useState, useEffect } from 'react'
 import axios from 'axios'
+const api_key = import.meta.env.VITE_SOME_KEY
 
 const Search = (props) => {
   return (
@@ -10,9 +11,9 @@ const Search = (props) => {
 }
 
 const Countries = (props) => {
-  if (props.countries){
+  if (props.countries.length > 0){
     if (props.countries.length === 1){
-      if (!props.country) { 
+      if (props.country.length <= 0) { 
         return null
       }
 
@@ -24,6 +25,10 @@ const Countries = (props) => {
           <h2>Languages</h2>
           <p>{props.country[1].map(name => <li key={name}>{name}</li>)}</p>
           <img src={props.country[3]} />
+          <h2>Weather in {props.country[2]}</h2>
+          <p>Temperature {props.weather[0]} Celcius</p>
+          <img src={props.weather[2]} />
+          <p>Wind {props.weather[1]} m/s</p>
         </div>
       )
     }
@@ -47,8 +52,9 @@ const Countries = (props) => {
 
 const App = () => {
   const [value, setValue] = useState('')
-  const [countries, setCountries] = useState(null)
-  const [country, setCountry] = useState(null)
+  const [countries, setCountries] = useState([])
+  const [country, setCountry] = useState([])
+  const [weather, setWeather] = useState([])
 
   const handleChange = (event) => {
     setValue(event.target.value)
@@ -66,38 +72,51 @@ const App = () => {
             setCountries(filtered)
           } else if (filtered.length < 11) {
             setCountries(filtered)
-            setCountry(null)
+            setCountry([])
           } else {
-            setCountries(null)
+            setCountries([])
           }
         })
     } else {
-      setCountries(null)
+      setCountries([])
     }
   }, [value])
 
-  const countryCheck = (country) => {
-    if (country) {
+  const countryCheck = (targetCountry) => {
+    if (targetCountry) {
         axios
-          .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${country}`)
+          .get(`https://studies.cs.helsinki.fi/restcountries/api/name/${targetCountry}`)
           .then(response => {
             const countryName = response.data.name.common
             const languages = Object.values(response.data.languages)
             const capital = response.data.capital
             const area = response.data.area
             const flag = response.data.flags.png
-            const information = [countryName, languages, capital, flag, area]
+            const lat = response.data.capitalInfo.latlng[0]
+            const lng = response.data.capitalInfo.latlng[1]
+            const information = [countryName, languages, capital, flag, area, lat, lng]
             setCountry(information)
+
+            axios
+            .get(`https://api.openweathermap.org/data/2.5/weather?lat=${information[5]}&lon=${information[6]}&appid=${api_key}`)
+            .then(response => {
+              const temperatureNow = Math.round((response.data.main.temp - 273.15) * 100) / 100
+              const wind = response.data.wind.speed
+              const weatherIcon = response.data.weather[0].icon
+              const iconLink = `https://openweathermap.org/img/wn/${weatherIcon}@2x.png`
+              const weatherInformation = [temperatureNow, wind, iconLink]
+              setWeather(weatherInformation)
+            })
           })
       } else {
-        setCountry(null)
+        setCountry([])
       }
   }
 
   return (
     <div>
       <Search value={value} handleChange={handleChange} />
-      <Countries country={country} countries={countries} setValue={setValue} />
+      <Countries country={country} countries={countries} setValue={setValue} weather={weather} />
     </div>
   )
 }
