8cbad63089a78748aa01ebba498f79bd334b2e05
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-09-16 12:47:46 +0300

    4.4: yksikkötestejä

--- osa4/blogilista/utils/list_helper.js
+++ osa4/blogilista/utils/list_helper.js
@@ -2,6 +2,18 @@ const dummy = (blogs) => {
   return 1
 }
 
+const totalLikes = (blogs) => {
+  if (blogs.length == 0) {
+    return 0
+  } else if (blogs.length == 1) {
+    return blogs[0].likes
+  } else if (blogs.length > 1) {
+    const initial = 0
+    return blogs.reduce((result, blog) => result + blog.likes, initial)
+  }
+}
+
 module.exports = {
-  dummy
+  dummy,
+  totalLikes
 }
