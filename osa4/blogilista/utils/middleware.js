154f879691882e93372fe8a9c9cd31582d4535d2
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-10-09 18:38:02 +0300

    4.20: blogilistan laajennus

--- osa4/blogilista/utils/middleware.js
+++ osa4/blogilista/utils/middleware.js
@@ -22,4 +22,15 @@ const errorHandler = (error, request, response, next) => {
   next(error)
 }
 
-module.exports = { errorHandler }
+const tokenExtractor = (request, response, next) => {
+  const authorization = request.get('authorization')
+  if (authorization && authorization.startsWith('Bearer ')) {
+    request.token = authorization.replace('Bearer ', '')
+  } else {
+    request.token = null
+  }
+
+  next()
+}
+
+module.exports = { errorHandler, tokenExtractor }
