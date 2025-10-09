e886a354fcd9ae6650162627daa6ca449dd8483f
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-10-09 14:13:57 +0300

    4.18: blogilistan laajennus

--- osa4/blogilista/app.js
+++ osa4/blogilista/app.js
@@ -3,6 +3,7 @@ const mongoose = require('mongoose')
 const config = require('./utils/config')
 const blogsRouter = require('./controllers/blogs')
 const usersRouter = require('./controllers/users')
+const loginRouter = require('./controllers/login')
 const middleware = require('./utils/middleware')
 const logger = require('./utils/logger')
 
@@ -20,6 +21,7 @@ app.use(express.json())
 
 app.use('/api/blogs', blogsRouter)
 app.use('/api/users', usersRouter)
+app.use('/api/login', loginRouter)
 
 app.use(middleware.errorHandler)
 
