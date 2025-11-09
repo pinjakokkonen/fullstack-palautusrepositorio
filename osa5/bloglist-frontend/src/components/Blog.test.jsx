d82e1abd47abe83742df015851cbba04a0a94ece
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-11-09 15:18:36 +0200

    5.14: testit

--- osa5/bloglist-frontend/src/components/Blog.test.jsx
+++ osa5/bloglist-frontend/src/components/Blog.test.jsx
@@ -1,4 +1,5 @@
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Blog from './Blog'
 
 test('renders title', () => {
@@ -14,4 +15,30 @@ test('renders title', () => {
 
   const element = screen.getByText('title author')
   expect(element).toBeDefined()
+})
+
+test('when clicking view button everything is shown', async () => {
+  const blog = {
+    title: 'title',
+    author: 'author',
+    url: 'url',
+    likes: 0,
+    user: { name: 'user' }
+  }
+
+  render(<Blog blog={blog} />)
+
+  const element1 = screen.getByText('url')
+  expect(element1).not.toBeVisible()
+
+  const user = userEvent.setup()
+  const button = screen.getByText('view')
+  await user.click(button)
+
+  const element2 = screen.getByText('url')
+  expect(element2).toBeVisible()
+  const element3 = screen.findByText(0)
+  expect(element3).toBeDefined()
+  const element4 = screen.getByText('user')
+  expect(element4).toBeVisible()
 })
