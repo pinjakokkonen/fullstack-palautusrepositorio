40d3211954cee7110ec661b1f066cfb6fb9fe163
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-11-22 11:26:12 +0200

    6.12: anekdootit

---
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -0,0 +1,13 @@
+import { createSlice } from "@reduxjs/toolkit"
+
+const notificationSlice = createSlice({
+  name: 'notification',
+  initialState: '',
+  reducers: {
+    showNotification(state, action) {
+      return action.payload
+    }
+  }
+})
+
+export default notificationSlice.reducer
