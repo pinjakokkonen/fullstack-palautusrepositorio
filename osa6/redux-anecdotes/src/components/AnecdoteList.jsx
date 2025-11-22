be1953e8db8d7e39675d650d9091d0b1636bce40
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-11-22 12:45:54 +0200

    6.13: anekdootit

--- osa6/redux-anecdotes/src/components/AnecdoteList.jsx
+++ osa6/redux-anecdotes/src/components/AnecdoteList.jsx
@@ -1,5 +1,6 @@
 import { useDispatch, useSelector } from 'react-redux'
 import { voteAnecdote } from '../reducers/anecdoteReducer'
+import { showNotification, removeNotification } from "../reducers/notificationReducer"
 
 const AnecdoteList = () => {
   const anecdotes = useSelector(state => {
@@ -10,9 +11,13 @@ const AnecdoteList = () => {
   })
   const dispatch = useDispatch()
 
-  const vote = id => {
+  const vote = (id, content) => {
     console.log('vote', id)
     dispatch(voteAnecdote(id))
+    dispatch(showNotification(content))
+    setTimeout(() => {
+      dispatch(removeNotification(''))
+    }, 5000)
   }
 
   const sortedAnecdotes = Object.values(anecdotes).sort((a, b) => b.votes - a.votes)
@@ -24,7 +29,7 @@ const AnecdoteList = () => {
         <div>{anecdote.content}</div>
         <div>
           has {anecdote.votes}
-          <button onClick={() => vote(anecdote.id)}>vote</button>
+          <button onClick={() => vote(anecdote.id, anecdote.content)}>vote</button>
         </div>
         </div>
       ))}
--- osa6/redux-anecdotes/src/reducers/notificationReducer.js
+++ osa6/redux-anecdotes/src/reducers/notificationReducer.js
@@ -5,9 +5,14 @@ const notificationSlice = createSlice({
   initialState: '',
   reducers: {
     showNotification(state, action) {
+      const content = `You voted ${action.payload}`
+      return content
+    },
+    removeNotification(state, action) {
       return action.payload
     }
-  }
+  },
 })
 
+export const { showNotification, removeNotification } = notificationSlice.actions
 export default notificationSlice.reducer
