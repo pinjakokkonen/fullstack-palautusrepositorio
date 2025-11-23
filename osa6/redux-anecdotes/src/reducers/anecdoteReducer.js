85917199d07e8e633087072c2e3005a2f203fb1c
Pinja Kokkonen <pinja.o.kokkonen@gmail.com>
2025-11-23 13:28:41 +0200

    6.14: backend

--- osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
+++ osa6/redux-anecdotes/src/reducers/anecdoteReducer.js
@@ -24,7 +24,7 @@ const initialState = anecdotesAtStart.map(asObject)
 
 const anecdoteSlice = createSlice({
   name: 'anecdotes',
-  initialState,
+  initialState: [],
   reducers: {
     createAnecdote(state, action) {
       const content = action.payload
@@ -39,9 +39,12 @@ const anecdoteSlice = createSlice({
       const votedAnecdote = state.filter(one => one.id===id)[0]
       const changeVote = {...votedAnecdote, votes: votedAnecdote.votes+1}
       return state.map(anecdote => (anecdote.id !== id ? anecdote : changeVote))
+    },
+    setAnecdotes(state, action) {
+      return action.payload
     }
   }
 })
 
-export const { createAnecdote, voteAnecdote } = anecdoteSlice.actions
+export const { createAnecdote, voteAnecdote, setAnecdotes } = anecdoteSlice.actions
 export default anecdoteSlice.reducer
